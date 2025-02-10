8b160f702af3b2335187c5cd47425ce6803aaa73
DavidFarewell <david.castillo.architect@gmail.com>
2025-02-10 18:39:03 +0000

    Generador de Cartas Aleatorias - David Farewell

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    let cardNumbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let suits = ["Diamonds", "Spades", "Hearts", "Clubs"];
+
+    let randomCardIndex = Math.floor(Math.random() * cardNumbers.length);
+    let randomSuitIndex = Math.floor(Math.random() * suits.length);
+    let selectedSuit = suits[randomSuitIndex];
+
+    document.getElementById('cardContent').innerHTML = cardNumbers[randomCardIndex];
+    let cardElement = document.getElementById('theCard');
+    cardElement.className = "card " + getSuitClass(selectedSuit);
+
+    console.log("Generated card: " + cardNumbers[randomCardIndex] + " of " + selectedSuit);
 };
+
+function getSuitClass(suit) {
+    switch (suit) {
+        case "Diamonds": return "suit-diamonds";
+        case "Spades": return "suit-spades";
+        case "Hearts": return "suit-hearts";
+        case "Clubs": return "suit-clubs";
+    }
+}
